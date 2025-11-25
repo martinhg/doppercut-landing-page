cfb7463e9b277d44487bb2179e1bbcdc9d58d419
Martin Herrera <martinherrera.g96@gmail.com>
2025-11-24 21:13:16 -0300

    SEO y Open Graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap'; // ¡Nuevo!
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://doppercut.com',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap(), robotsTxt()],
 });
